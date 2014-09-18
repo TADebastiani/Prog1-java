fa3a2f9b28768d5c2b9bb6c3a2ac4b0dae1c3627
Tiago Debastiani <debastianister@gmail.com>
2014-09-18 20:28:32 -0300

    adicionado novos metodos

--- FormaMain.java
+++ FormaMain.java
@@ -5,7 +5,7 @@ class FormaMain{
 		Scanner sc = new Scanner(System.in);
 
 		Quadrado caixa = new Quadrado();
-		
+		Quadrado vetor[] = new Quadrado[10];
 
 		System.out.println("Defina X:");
 		caixa.setX(sc.nextInt());
@@ -14,23 +14,16 @@ class FormaMain{
 		System.out.println("Defina Lado:");
 		caixa.setLado(sc.nextInt());
 
-		System.out.println("(" + caixa.getX() + ", " + caixa.getY() + ") Lado: " + caixa.getLado());
+		for (int i=0; i<vetor.length; i++){
+			
+			vetor[i].setX(caixa.getX());
+			vetor[i].setY(caixa.getY());
+			vetor[i].setLado(caixa.getLado());
+		}
 
-		for (int i = 0; i <= caixa.getY(); i++){
-			System.out.println();
-		}
-		for (int i = 0; i<= caixa.getLado(); i++){
-			for (int j = 0; j<= caixa.getLado(); j++) {
-				if (j == 0){
-					for (int abacate = 0; abacate<= caixa.getX(); abacate++){
-						System.out.print("  ");
-					}
-					System.out.print("#");
-				}else{
-					System.out.print(" #");
-				}
-			}
-			System.out.println();
-		}
+		caixa.imprimeQuadrado();
+
+		Quadrado.imprimeVetor(vetor);
+
 	}
 }
--- Quadrado.java
+++ Quadrado.java
@@ -10,9 +10,8 @@ class Quadrado extends FormaGeo{
 		return this.lado;
 	}
 
-
-	public int quantosMaiores(Quadrado[] v){
-		int count;
+	public int quantosMaiores(Quadrado v[]){
+		int count=0;
 
 		for (int i=0; i<=v.length; i++) {
 			if (v[i].getLado() > 20){
@@ -21,5 +20,33 @@ class Quadrado extends FormaGeo{
 		}
 
 		return count;
+	}
+
+	public void imprimeQuadrado(){
+		System.out.println("(" + this.getX() + ", " + this.getY() + ") Lado: " + this.getLado());
+
+		for (int i = 0; i <= this.getY(); i++){
+			System.out.println();
+		}
+		for (int i = 0; i<= this.getLado(); i++){
+			for (int j = 0; j<= this.getLado(); j++) {
+				if (j == 0){
+					for (int abacate = 0; abacate<= this.getX(); abacate++){
+						System.out.print("  ");
+					}
+					System.out.print("#");
+				}else{
+					System.out.print(" #");
+				}
+			}
+			System.out.println();
+		}	
+	}
+
+	public static void imprimeVetor(Quadrado v[]){
+		for (int i=0; i<v.length; i++) {
+			System.out.println("Vetor "+i+"("+v[i].getX()+","+v[i].getY()+"): ");
+			System.out.println("Lado: " + v[i].getLado());
+		}
 	}
 }
